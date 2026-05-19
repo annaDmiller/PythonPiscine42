6a8441efd6b18bc814db5dee31f53ed8207ca70d
Anna Melnikova <amelniko@student.42.fr>
2026-05-19 20:15:26 +0200

    Exercise 04 is done

---
+++ Day00/ex04/whatis.py
@@ -0,0 +1,31 @@
+import sys as sys
+
+args = sys.argv[1:]
+num_args = len(args)
+
+if num_args > 1:
+    print("AssertionError: more than one argument is provided")
+    exit(1)
+
+if num_args == 0:
+    print("")
+    exit(0)
+
+i = 0
+while i < len(args[0]):
+    char = args[0][i]
+    
+    if char < '0' or char > '9':
+        if char == '-' and i == 0:
+            i += 1
+            continue
+        print("AssertionError: argument is not an integer")
+        exit(1)
+
+    i += 1
+
+number = int(args[0])
+if number % 2 == 0:
+    print("I'm Even.")
+else:
+    print("I'm Odd.")
