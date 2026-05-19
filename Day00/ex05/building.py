ee7be0e1b83aa589d0dbc9f08400b5e2cd1110ce
Anna Melnikova <amelniko@student.42.fr>
2026-05-19 22:02:32 +0200

    Update of the ex05 file with python-type code and with avoiding crashes while inputing from std read

--- Day00/ex05/building.py
+++ Day00/ex05/building.py
@@ -1,8 +1,6 @@
 import sys as sys
 
 #add ___doc___
-#process Ctrl + C exception
-#find a better way to write function (?)
 
 def main():
     PUNCT = r"""!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"""
@@ -12,47 +10,33 @@ def main():
 
     if size_args > 1:
         print("AssertionError: more than one argument is provided")
-        exit(1)
+        return 
     
     if size_args == 0:
         print("What is the next to count?")
-        line = sys.stdin.read()
+        try:
+            line = sys.stdin.read()
+        except:
+            print("An exception occured")
+            return 
         
     else:
         line = args[0]
 
-    num_upper = 0
-    num_lower = 0
-    num_punct = 0
-    num_spaces = 0
-    num_digits = 0
-    num_chars = 0
+    num_upper = sum(ch.isupper() for ch in line)
+    num_lower = sum(ch.islower() for ch in line)
+    num_punct = sum(ch in PUNCT for ch in line)
+    num_spaces = sum(ch in ' \n\r\t\v\f' for ch in line)
+    num_digits = sum(ch.isdigit() for ch in line)
 
-    for char in line:
-        num_chars += 1
-        if char >= '0' and char <= '9':
-            num_digits += 1
-
-        elif char >= 'a' and char <= 'z':
-            num_lower += 1
-        
-        elif char >= 'A' and char <= 'Z':
-            num_upper += 1
-
-        elif char == ' ' or char == '\r' or char == '\n':
-            num_spaces += 1
-
-        elif char in PUNCT:
-            num_punct += 1
-
-    print("The text contains", num_chars, "characters:")
+    print("The text contains", len(line), "characters:")
     print(num_upper, "upper letters")
     print(num_lower, "lower letters")
     print(num_punct, "punctuation marks")
     print(num_spaces, "spaces")
     print(num_digits, "digits")
 
-    exit(0)
+    return 
 
 if __name__ == "__main__":
     main()    
