c2232ecf6cbfb744175740feca742179ba633f58
Anna Melnikova <amelniko@student.42.fr>
2026-06-02 21:37:12 +0200

    Fix of the documentation for the functions

--- Day00/ex06/filterstring.py
+++ Day00/ex06/filterstring.py
@@ -1,6 +1,11 @@
 import sys as sys
 
 def main():
+    """
+    Read two arguments - a string and an integer - and prints number of words from string that have a length greater than an integer number.
+
+    If the number of arguments is different from 2, or if the type of any argument is wrong, prints AssertionError.
+    """
     args = sys.argv[1:]
     args_num = len(args)
 
--- Day00/ex06/ft_filter.py
+++ Day00/ex06/ft_filter.py
@@ -1,5 +1,5 @@
 def ft_filter(function, iterable):
-    """filter(function or None, iterable) --> filter object
+    """ft_filter(function or None, iterable) --> list object
 
     Return an iterator yielding those items of iterable for which function(item) is true. If function is None, return the items that are true.
     """
