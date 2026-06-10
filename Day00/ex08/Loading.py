5b8928b06f408bffc13b7760b047e0ba9478c6ef
Anna Melnikova <amelniko@student.42.fr>
2026-06-10 23:33:00 +0200

    Ex08 is done + Ex06: function descriptions are fixed a bit

---
+++ Day00/ex08/Loading.py
@@ -0,0 +1,37 @@
+import os as os
+
+
+def ft_tqdm(lst: range) -> None:
+    """
+    ft_tqdm(lst: range) --> None
+
+    Prints a progress bar for the defined lst range similar to the one
+    of tqdm function but without elapsed time, estimated remaining time
+    and speed in iteractions per second.
+    """
+    total = len(lst)
+    terminal_width = os.get_terminal_size().columns
+    left = len(f"100%|| {total}/{total}[00:01<00:00, 191.61it/s]")
+    bar_len = terminal_width - left - 1
+
+    if bar_len < 1:
+        bar_len = 1
+
+    for i, item in enumerate(lst, 1):
+        percent = int(i / total * 100)
+        filled_part = int((i / total) * bar_len)
+
+        if i < total:
+            if filled_part > 0:
+                bar = "█" * (filled_part - 1) + " " * (bar_len - filled_part)
+            else:
+                bar = " " * (bar_len - 1)
+        else:
+            bar = "█" * bar_len
+
+        total_line = f"\r{percent}%|{bar}| {i}/{total}"
+        os.write(1, total_line.encode())
+        yield item
+
+    os.write(1, b"\n")
+    return None
