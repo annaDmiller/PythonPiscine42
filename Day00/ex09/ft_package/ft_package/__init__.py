a941822bf685c78209b867148de707d6c81a6077
Anna Melnikova <amelniko@student.42.fr>
2026-06-11 22:11:48 +0200

    Ex09 (package) is done????

---
+++ Day00/ex09/ft_package/ft_package/__init__.py
@@ -0,0 +1,11 @@
+from .ft_filter import ft_filter
+
+
+def count_in_list(lst: list, value) -> int:
+    """
+    count_in_list(lst: list, value) -> int
+
+    Function returns the quantity of value appearance in list lst.
+    """
+
+    return lst.count(value)
