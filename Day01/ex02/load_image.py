abf869974a479b5c69ba01aa275c1955ab89f2c6
Anna Melnikova <amelniko@student.42.fr>
2026-06-20 15:19:49 +0200

    Update of ex02 files

--- Day01/ex02/load_image.py
+++ Day01/ex02/load_image.py
@@ -1,5 +1,6 @@
 from numpy import asarray, array
 from PIL import Image as img
+from PIL import UnidentifiedImageError
 
 
 def ft_load(path: str) -> array:
@@ -13,9 +14,15 @@ def ft_load(path: str) -> array:
     try:
         image = img.open(path)
     except FileNotFoundError:
-        raise FileNotFoundError(f"File not found with this path: {path}")
+        raise FileNotFoundError(f"File not found: {path}") from None
+    except PermissionError:
+        raise PermissionError(f"No access to the file: {path}") from None
+    except IsADirectoryError:
+        raise IsADirectoryError(f"This is a directory: {path}") from None
+    except UnidentifiedImageError:
+        raise UnidentifiedImageError("Incorrect format of image.") from None
     except OSError:
-        raise ValueError("Invalid image file or unsupported format.")
+        raise OSError("OS error occured. Try once more later.") from None
 
     if image.format not in ("JPEG", "JPG"):
         raise ValueError("Only JPG and JPED formats are supported.")
--- Day01/ex02/tester.py
+++ Day01/ex02/tester.py
@@ -1,3 +1,8 @@
 from load_image import ft_load
 
 print(ft_load("landscape.jpg"))
+#print(ft_load("animal.jpeg"))
+print(ft_load("animal1.jpeg"))
+#print(ft_load("../ex02"))
+#print(ft_load("test_txt.txt"))
+#print(ft_load("png-clipart-spider-man-heroes-download-with-transparent-background-thumbnail.png"))
