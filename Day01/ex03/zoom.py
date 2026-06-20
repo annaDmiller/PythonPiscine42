0cbeb6200ac5fb4c5704b503b5f841842027a725
Anna Melnikova <amelniko@student.42.fr>
2026-06-20 17:33:43 +0200

    Testing ex03

---
+++ Day01/ex03/zoom.py
@@ -0,0 +1,29 @@
+from load_image import ft_load
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def main():
+    """
+    Loads an image "animal.jpeg", prints information about it,
+    and displays it after zooming.
+    """
+    image_path = "animal.jpeg"
+    try:
+        rgb_arr = ft_load(image_path)
+        print(rgb_arr)
+
+        zoomed_arr = rgb_arr[100:500, 100:500, 0:1]
+        print("New shape after slicing:", zoomed_arr.shape)
+        print(zoomed_arr)
+
+        plt.imshow(zoomed_arr[:, :, 0])
+        plt.show()
+    except Exception as e:
+        print(f"Error: {e}")
+    return
+
+
+
+if __name__ == "__main__":
+    main()
