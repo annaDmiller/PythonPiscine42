16fddee5c850d04e1bcbda4c0b911c7fc653fa1e
amelniko <amelniko@f6r8s14.paris.42.school>
2026-06-27 16:08:15 +0200

    Fix of ex01 according to flake8

--- Day02/ex01/aff_life.py
+++ Day02/ex01/aff_life.py
@@ -1,6 +1,4 @@
 from load_csv import load
-from pandas import DataFrame as Dataset
-import pandas as pd
 import matplotlib.pyplot as plt
 
 
@@ -11,13 +9,13 @@ def main():
     """
     data = load("life_expectancy_years.csv")
     if data is None:
-        return 
+        return
 
     campus_country = "France"
     if campus_country not in data.index:
         print(f"No data for the specified country: {campus_country}")
         return
-    
+
     country_data = data.loc[campus_country]
     years = country_data.index.astype(int)
     values = country_data.values.astype(float)
