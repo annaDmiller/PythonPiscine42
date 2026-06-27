e8612de1f0e6630b5d90f6064cd056bc22c8e1b9
amelniko <amelniko@f6r8s14.paris.42.school>
2026-06-27 18:56:28 +0200

    Ex03 is done

--- Day02/ex03/projection_life.py
+++ Day02/ex03/projection_life.py
@@ -9,11 +9,24 @@ def main():
     the year 1900 for each country.
     """
     income = load("income_per_person_gdppercapita_ppp_inflation_adjusted.csv")
-    years = load("life_expectancy_years.csv")
-    if data is None or years is None:
+    life_expect = load("life_expectancy_years.csv")
+    if income is None or life_expect is None:
         return
-    
 
+    year_income = income["1900"].astype(int)
+    year_life = life_expect["1900"].astype(float)
+
+    plt.scatter(year_income, year_life, label="1900")
+    plt.title("1900")
+
+    plt.xlabel("Gross domestic product")
+    plt.xscale("log")
+    xticks = [300, 1000, 10000]
+    xlabels = ["300", "1k", "10k"]
+    plt.xticks(xticks, xlabels)
+
+    plt.ylabel("Life expectancy")
+    plt.show()
 
 
 if __name__ == "__main__":
