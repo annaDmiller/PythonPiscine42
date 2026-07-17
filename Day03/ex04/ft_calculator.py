2ae07b70cb4c56d0dbed854c2bce817dfce5b748
Anna Melnikova <amelniko@student.42.fr>
2026-07-17 21:05:23 +0200

    Start of development of ex04

---
+++ Day03/ex04/ft_calculator.py
@@ -0,0 +1,14 @@
+class calculator:
+#your code here
+
+    # decorator
+    def dotproduct(V1: list[float], V2: list[float]) -> None:
+        pass
+
+    # decorator
+    def add_vec(V1: list[float], V2: list[float]) -> None:
+        pass
+
+    # decorator
+    def sous_vec(V1: list[float], V2: list[float]) -> None:
+        pass
