d0819b0b7ba4c3e873d20996de96d0508d4b3a4a
Anna Melnikova <amelniko@student.42.fr>
2026-07-17 19:24:16 +0200

    ex02 is done

--- Day03/ex02/DiamondTrap.py
+++ Day03/ex02/DiamondTrap.py
@@ -1,4 +1,25 @@
 from S1E7 import Baratheon, Lannister
 
+
 class King(Baratheon, Lannister):
-    pass
+    """A king that inherits from 2 families: Baratheon and Lannister."""
+    def __init__(self, first_name: str, is_alive: bool = True):
+        """Initializes the King. Takes first_name and is_alive
+        (non-mandatory, default=True) as paramters."""
+        super().__init__(first_name, is_alive)
+
+    def set_eyes(self, eyes_color: str):
+        """Set the eyes color with the string value given as parameter."""
+        self.eyes = eyes_color
+
+    def set_hairs(self, hairs_color: str):
+        """Set the hairs color with the string value given as parameter."""
+        self.hairs = hairs_color
+
+    def get_eyes(self) -> str:
+        """Returns a string with value of the eyes color."""
+        return self.eyes
+
+    def get_hairs(self) -> str:
+        """Returns a string with value of the hairs color."""
+        return self.hairs
