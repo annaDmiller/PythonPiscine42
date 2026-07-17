27d640e87279d7afd50f146c75f426ec4f0f633e
Anna Melnikova <amelniko@student.42.fr>
2026-07-17 21:23:31 +0200

    ex04 is done

--- Day03/ex04/ft_calculator.py
+++ Day03/ex04/ft_calculator.py
@@ -11,19 +11,23 @@ class calculator:
     def dotproduct(V1: list[float], V2: list[float]) -> None:
         """dotproduct(V1: list[float], V2: list[float]) -> None
 
-        """
-        pass
+        Calculates the dot product of two vectors."""
+        temp = [a * b for a, b in zip(V1, V2)]
+        res = sum(x for x in temp)
+        print(f"Dot product is : {res}")
 
     @staticmethod
     def add_vec(V1: list[float], V2: list[float]) -> None:
         """add_vec(V1: list[float], V2: list[float]) -> None
 
-        """
-        pass
+        Calculates the result of addition of two vectors."""
+        res = [float(a + b) for a, b in zip(V1, V2)]
+        print(f"Add Vector is : {res}")
 
     @staticmethod
     def sous_vec(V1: list[float], V2: list[float]) -> None:
         """sous_vec(V1: list[float], V2: list[float]) -> None
 
-        """
-        pass
+        Calculates the result of substraction of two vectors."""
+        res = [float(a - b) for a, b in zip(V1, V2)]
+        print(f"Sous Vector is : {res}")
